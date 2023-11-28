ee017d676ec10e7e395ec9c5ca37a6d0f045fda6
Pallavi-0880 <pallavithakur0880@gmail.com>
2023-11-28 15:43:50 +0530

    code review changes

--- cards.js
+++
@@ -1,7 +0,0 @@
-let container = document.querySelector(".container")
-for (var i = 1; i < 26; i++) {
-		var cell = document.createElement("div");
-		cell.classList.add("cell");
-		container.appendChild(cell);
-	}
-
